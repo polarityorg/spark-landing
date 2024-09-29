1c8d6f69afdf4f3a0f822f05caec399cfed22905
ethanmarcuss <ethan@marcus.me>
2024-09-29 16:23:33 -0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -13,8 +13,25 @@ export default function Home({
     searchParams.technical !== undefined ? "technical" : "overview";
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen max-w-3xl mx-auto p-8 pb-20 gap-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 sm:items-start">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen max-w-3xl mx-auto p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 pb-8 row-start-2 sm:items-start">
+        <p className="text-sm text-left text-muted-foreground sm:text-left font-[family-name:var(--font-geist-mono)]">
+          A{" "}
+          <a
+            href="https://lightspark.com"
+            className="hover:underline hover:underline-offset-4"
+            target="_blank">
+            Lightspark
+          </a>{" "}
+          &{" "}
+          <a
+            href="https://flashnet.xyz"
+            className="hover:underline hover:underline-offset-4"
+            target="_blank">
+            Polarity
+          </a>{" "}
+          initiative
+        </p>
         <Image
           className="dark:invert"
           src="/spark.svg"
@@ -23,7 +40,7 @@ export default function Home({
           height={38}
           priority
         />
-        <p className="list-inside list-decimal text-sm text-left sm:text-left font-[family-name:var(--font-geist-mono)]">
+        <p className="text-sm text-left sm:text-left font-[family-name:var(--font-geist-mono)]">
           A trust-minimized solution designed to scale Bitcoin and extend the
           Lightning Network.
         </p>
