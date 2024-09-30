17c29d4f3e49b7cf23660d63e3bf5a9bb1591dea
ethanmarcuss <ethan@marcus.me>
2024-09-30 15:03:48 -0700

    Lint errors

--- components/phone-input.tsx
+++ components/phone-input.tsx
@@ -15,7 +15,7 @@ import {
   CommandItem,
   CommandList,
 } from "@/components/ui/command";
-import { Input, InputProps } from "@/components/ui/input";
+import { Input } from "@/components/ui/input";
 import {
   Popover,
   PopoverContent,
@@ -52,18 +52,19 @@ const PhoneInput: React.ForwardRefExoticComponent<PhoneInputProps> =
   );
 PhoneInput.displayName = "PhoneInput";
 
-const InputComponent = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, ...props }, ref) => (
-    <Input
-      className={cn(
-        "rounded-e-lg rounded-s-none text-base font-medium py-6 px-4 shadow-none",
-        className
-      )}
-      {...props}
-      ref={ref}
-    />
-  )
-);
+const InputComponent = React.forwardRef<
+  HTMLInputElement,
+  React.InputHTMLAttributes<HTMLInputElement>
+>(({ className, ...props }, ref) => (
+  <Input
+    className={cn(
+      "rounded-e-lg rounded-s-none text-base font-medium py-6 px-4 shadow-none",
+      className
+    )}
+    {...props}
+    ref={ref}
+  />
+));
 InputComponent.displayName = "InputComponent";
 
 type CountrySelectOption = { label: string; value: RPNInput.Country };
