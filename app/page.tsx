ad7f38e478f57c847174efe287db76679dbf067a
ethanmarcuss <ethan@marcus.me>
2024-09-30 09:37:17 -0700

    remove tagline temp

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home({
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen max-w-3xl mx-auto p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 pb-8 row-start-2 sm:items-start">
-        <p className="text-sm text-left text-muted-foreground sm:text-left font-[family-name:var(--font-geist-mono)]">
+        {/* <p className="text-sm text-left text-muted-foreground sm:text-left font-[family-name:var(--font-geist-mono)]">
           A{" "}
           <a
             href="https://lightspark.com"
@@ -31,7 +31,7 @@ export default function Home({
             Polarity
           </a>{" "}
           initiative
-        </p>
+        </p> */}
         <Image
           className="dark:invert"
           src="/spark.svg"
