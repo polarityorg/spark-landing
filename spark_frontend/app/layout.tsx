fec5f42451e8ec47d5d855bdea826e71ba3cb4a0
ethanmarcuss <ethan@marcus.me>
2024-10-19 16:40:12 -0700

    Try new icons

--- spark_frontend/app/layout.tsx
+++ spark_frontend/app/layout.tsx
@@ -43,6 +43,37 @@ export default function RootLayout({
           content="black-translucent"
         />
 
+        {/* Web App Manifest */}
+        <link rel="manifest" href="/manifest.json" />
+
+        {/* iOS Support */}
+        <link rel="apple-touch-icon" href="/icon192.png" />
+        <link rel="apple-touch-icon" href="/icon512.png" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+
+        {/* Splash Screens */}
+        <link
+          rel="apple-touch-startup-image"
+          href="/apple-splash-640-1136.png"
+          media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/apple-splash-750-1334.png"
+          media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/apple-splash-1242-2208.png"
+          media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/apple-splash-1125-2436.png"
+          media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
+        />
+
         {/* Disable Zoom Script */}
         <Script id="disable-zoom" strategy="beforeInteractive">
           {`
